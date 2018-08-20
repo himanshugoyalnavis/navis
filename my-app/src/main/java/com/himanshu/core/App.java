3498704a46f0c9ad075d90d2bdfca05b468622b9
himanshu.goyal <himanshu.goyal@navis.com>
2018-08-20 17:35:43 +0530

    himanshu's app

---
+++ my-app/src/main/java/com/himanshu/core/App.java
@@ -0,0 +1,13 @@
+package com.himanshu.core;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println("Created By Himanshu Goyal");
+    }
+}
